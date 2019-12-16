2ba76bed1df7d8e72840d71a54779f0ad6d02898
Aakash Babu Shrestha <sthaakash.1995@gmail.com>
2019-12-16 12:17:41 +0545

    Added Scene Manager

---
+++ F.I.R.S.T/Assets/Script/SceneManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SceneManager : MonoBehaviour
+{
+    
+    public void Home()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Home");
+    }
+
+    public void ModeSelect()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("ModeSelect");
+    }
+
+    public void Snake()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Snake");
+    }
+
+    public void PreviousScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("ModeSelect");
+    }
+}
