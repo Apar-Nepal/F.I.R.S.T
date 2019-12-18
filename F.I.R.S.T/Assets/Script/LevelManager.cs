cc8c9cd305d9cadaf34fc8d0ce907316b3936779
Aakash Babu Shrestha <sthaakash.1995@gmail.com>
2019-12-18 11:36:40 +0545

    changed SceneManager into LevelManager

---
+++ F.I.R.S.T/Assets/Script/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class LevelManager : MonoBehaviour
+{
+    
+    public void Home()
+    {
+        SceneManager.LoadScene("Home");
+    }
+
+    public void ModeSelect()
+    {
+        SceneManager.LoadScene("ModeSelect");
+    }
+
+    public void Snake()
+    {
+        SceneManager.LoadScene("Snake");
+    }
+
+    public void PreviousScene()
+    {
+        SceneManager.LoadScene("ModeSelect");
+    }
+}
