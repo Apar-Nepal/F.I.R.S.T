e40372367126c0d348e1be902340fa6ccfbf27cd
Aakash Babu Shrestha <sthaakash.1995@gmail.com>
2019-12-30 15:51:30 +0545

    slider fot he game mode added

---
+++ F.I.R.S.T/Assets/SliderButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderButton : MonoBehaviour
+{
+    public Scrollbar sliderValue;
+   
+
+    private void Awake()
+    {
+        sliderValue = this.GetComponent<Scrollbar>();
+    }
+
+    public void ButtonLeft()
+    {
+        sliderValue.value = sliderValue.value - (float)0.50;
+    }
+
+    public void ButtonRight()
+    {
+        sliderValue.value = sliderValue.value + (float)0.50;
+    }
+}
