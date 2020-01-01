6aff44274868f7d172208ee3f454f74a9410d9fb
Aakash Babu Shrestha <sthaakash.1995@gmail.com>
2020-01-01 10:27:17 +0545

    Changes to the modelSelect Navigator

---
+++ F.I.R.S.T/Assets/Script/Navigator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Navigator : MonoBehaviour
+{
+    public Scrollbar sliderValue;
+    int scrollPage;
+
+    public GameObject pageNoText;
+    private TextMeshPro text;
+
+    private void Awake()
+    {
+        text = pageNoText.GetComponent<TextMeshPro>();
+        Debug.Log(text.text);
+        text.text = 1 + "";
+        scrollPage = 1;
+    }
+
+    public void ButtonLeft()
+    {
+        sliderValue.value = sliderValue.value - (float)0.50;
+        // change page
+        if (scrollPage > 1)
+        {
+            scrollPage--;
+        }
+
+        ChangePage();
+    }
+
+    public void ButtonRight()
+    {
+        sliderValue.value = sliderValue.value + (float)0.50;
+
+        if (scrollPage<3)
+        {
+            scrollPage++;
+        }
+
+        // change page number
+        ChangePage();
+    }
+
+    void ChangePage()
+    {
+        text.text = "" + scrollPage;
+    }
+}
