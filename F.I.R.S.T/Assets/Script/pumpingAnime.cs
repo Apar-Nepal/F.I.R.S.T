c2fcaa09e96e70386da9f2af636a73a393a122c7
Aakash Babu Shrestha <sthaakash.1995@gmail.com>
2020-01-06 15:46:05 +0545

    Changed the pointer used and started on UI

---
+++ F.I.R.S.T/Assets/Script/pumpingAnime.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pumpingAnime : MonoBehaviour
+{
+    public Animator anim;
+
+    public void AnimatePumping()
+    {
+        anim.SetTrigger("pumping");
+    }
+}
