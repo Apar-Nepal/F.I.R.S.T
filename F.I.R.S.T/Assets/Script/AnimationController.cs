bdf6fe550f85ff462cb889d2e99b16ddcad69c4c
Aakash Babu Shrestha <sthaakash.1995@gmail.com>
2020-01-07 14:19:15 +0545

    added new script for the animator

---
+++ F.I.R.S.T/Assets/Script/AnimationController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    public Animator anim;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        // check breathing for the victim
+        if (collision.collider.tag == "victimHead")
+        {
+            anim.SetTrigger("checkBreathing");
+        }
+
+        // start CPR
+        if (collision.collider.tag == "victimChest")
+        {
+            anim.SetTrigger("pumping");
+        }
+    }
+}
